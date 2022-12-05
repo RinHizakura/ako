b46c626bf6880903a2021789d541714d9a879377
RinHizakura <s921975628@gmail.com>
2022-12-05 14:58:56 +0800

    Refactor for integer token

--- src/lexer.rs
+++ src/lexer.rs
@@ -10,6 +10,19 @@ impl Lexer {
         Lexer { expr: expr, pos: 0 }
     }
 
+    fn gettoken_int(&mut self, mut n: usize) -> Option<Token> {
+        while let Some(next_ch) = self.expr.chars().nth(self.pos) {
+            if let Some(digit) = next_ch.to_digit(10) {
+                n *= 10;
+                n += digit as usize;
+                self.pos += 1;
+            } else {
+                break;
+            }
+        }
+        Some(Token::int(n))
+    }
+
     pub fn gettoken(&mut self) -> Option<Token> {
         if let Some(ch) = self.expr.chars().nth(self.pos) {
             self.pos += 1;
@@ -20,17 +33,7 @@ impl Lexer {
                 '/' => Some(Token::slash()),
                 _ => {
                     if let Some(n) = ch.to_digit(10) {
-                        let mut n = n as usize;
-                        while let Some(next_ch) = self.expr.chars().nth(self.pos) {
-                            if let Some(digit) = next_ch.to_digit(10) {
-                                n *= 10;
-                                n += digit as usize;
-                                self.pos += 1;
-                            } else {
-                                break;
-                            }
-                        }
-                        Some(Token::int(n))
+                        self.gettoken_int(n as usize)
                     } else {
                         // TODO: support variable name token
                         Some(Token::unknown())
